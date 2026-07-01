53a0e577fd2de97b37f6dec07cb751c33a1810b9
Culberthrules <culberthrules@github.com>
2026-07-01 15:09:28 +0100

    Update .gitignore and vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,7 @@ export default defineConfig({
     babel({ presets: [reactCompilerPreset()] })
   ],
   server: {
+    host: '127.0.0.1',
     proxy: {
       '/api': {
         target: 'http://localhost:5000',
